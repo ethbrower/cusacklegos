6809ebf2769b1a0c7c9d49af103ff4eb651a4a6c
Justin Fay <justin.fay@hope.edu>
2023-10-06 11:48:54 -0400

    Started vscode project version

---
+++ Cusack_Legos/BreakingTheCode/src/App.java
@@ -0,0 +1,11 @@
+package Cusack_Legos.BreakingTheCode.src;
+
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        HardCodedArrays squares = new HardCodedArrays();
+
+       squares.printSquareValues(squares.top_left);
+    }
+}
